09ba95ead360cddfbb6eb8e8c5988dad3776d211
Kalleb Dayan <kallebdayan@gmail.com>
2025-05-25 14:51:40 -0300

    Novos arquivos da s aulas

--- input.py
+++ input.py
@@ -1,2 +1,8 @@
-name = input("ENTRE CO SEU NOME")
-print(f"Seu nome é {name}")
+nome = input("Digite seu nome: ")
+
+# Mostrar cada letra em uma linha
+for letra in nome:
+    print(letra)
+
+# Mostrar o nome completo no final
+print(f"Seu nome é {nome}")
