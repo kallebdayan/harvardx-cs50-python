c4d31e1246f76b14a84cded3321b1a83d52997ce
Kalleb Dayan <kallebdayan@gmail.com>
2025-05-25 14:51:49 -0300

    Novos arquivos da s aulas

---
+++ sequences.py
@@ -0,0 +1,8 @@
+coordinateX = 10.0
+coordianteY = 20.0
+coordinates = (10.0,20.0)
+
+#	list 	- sequences of mutable values
+#	tuple 	- sequences of immutable values 
+#	set		- sequences of unique values
+#	dict	- sequences of key-values pair
